c15a12f1985684c0bfe9e7818b1881b3962af2ca
Lucas Huet-Hudson <lucashh@microsoft.com>
2021-04-23 15:31:36 -0400

    update product doc

--- src/inventory-api/inventory-api/Models/ProductDocument.cs
+++ src/inventory-api/inventory-api/Models/ProductDocument.cs
@@ -3,30 +3,15 @@ using Newtonsoft.Json;
 
 namespace Inventory.Models
 {
-    public class ProductDocument
-    {
-        [JsonProperty("id")]
-        public Guid Id { get; set; }
+  public class ProductDocument
+  {
+    [JsonProperty("id")]
+    public Guid Id { get; set; }
 
-        [JsonProperty("description")]
-        public string Description { get; set; }
+    [JsonProperty("description")]
+    public string Description { get; set; }
 
-        [JsonProperty("unitPrice")]
-        public double UnitPrice { get; set; }
-
-        [JsonProperty("category")]
-        public string Category { get; set; }
-
-        [JsonProperty("warehouses")]
-        public WarehouseAvailability[] Warehouses { get; set; }
-    }
-
-    public class WarehouseAvailability
-    {
-        [JsonProperty("warehouseId")]
-        public Guid WarehouseId { get; set; }
-
-        [JsonProperty("unitsAvailable")]
-        public int UnitsAvailable { get; set; }
-    }
+    [JsonProperty("unitPrice")]
+    public double UnitPrice { get; set; }
+  }
 }
